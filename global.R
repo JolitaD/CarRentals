42665b0687e60b49f475f87b9510fcf27224a0f0
JolitaD <jolita3@gmail.com>
2017-02-10 16:56:12 +0200

    Create global.R

---
+++ global.R
@@ -0,0 +1,7 @@
+# Variables that can be put on the x and y axes
+axis_vars <- c(
+  "Liko dienÅ³" = "Liko_dienu",
+  "MÄnuo" = "Menuo",
+  "Kaina"="Price",
+  "Liko dienÅ³ po 50"="Liko_dienu_po50"
+  )
